14740bb71aa4071fe76458371f6bbb7c97162903
Mark Ahrens <mark-ahrens@uiowa.edu>
2017-03-08 09:36:54 -0600

    Photoshelter slider

---
+++ modules/custom/osc_photoshelter/slider.js
@@ -0,0 +1,10 @@
+(function ($) {
+  $(window).load(function() {
+ 
+    $('.slider').flexslider({
+	    animation: "slide",
+	    animationLoop: true,
+	  });
+ 
+  }); // end window.load
+})(jQuery);
